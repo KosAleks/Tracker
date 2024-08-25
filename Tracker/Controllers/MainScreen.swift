0dbc3a38702bb4249ad86decd900f2cc2303c935
Aleksandra <aleksandra.kosnireva@yandex.ru>
2024-08-25 21:53:46 +0500

    deleted cleaner.

--- Tracker/Controllers/MainScreen.swift
+++ Tracker/Controllers/MainScreen.swift
@@ -60,7 +60,6 @@ final class MainScreen: UIViewController, UISearchBarDelegate {
         syncData()
         updateUI()
         hideKeyboardWhenTappedAround()
-        deleteAllTrackers() 
     }
     
     private func deleteAllTrackers() {
